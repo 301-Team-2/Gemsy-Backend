3db7bddafa555550c1dbbcb4c9b0edfab2c35cfe
joshuashea <joshshea44@gmail.com>
2023-09-04 19:31:16 -0700

    can get events data from api through local host server

--- events.js
+++ events.js
@@ -6,3 +6,23 @@ const axios = require('axios');
 const YELP_API_KEY = process.env.YELP_API_KEY;
 
 const yelpUrl = 'https://api.yelp.com/v3';
+
+const handleEventsRequest = async (req, res) => {
+  try {
+    const customHeaders = {
+      Authorization: `Bearer ${YELP_API_KEY}`,
+      'Content-Type': 'application/json',
+    };
+
+    const response = await axios.get(`${yelpUrl}/events`, {
+      headers: customHeaders,
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occured' });
+  }
+};
+
+module.exports = handleEventsRequest;
