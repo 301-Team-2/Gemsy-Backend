f2f776ac7943d3c15e93c563d48712e7f7161d90
joshuashea <joshshea44@gmail.com>
2023-09-04 17:11:33 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const mongoose = require('mongoose');
+
+PORT = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log('App is listening.');
+});
